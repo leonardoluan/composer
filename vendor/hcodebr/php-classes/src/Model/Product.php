c2df9138687a30da161084b3576e64e8da9e471c
Leonardo Luan <leo.luangg@outlook.com>
2018-06-05 17:48:27 -0300

    OK

--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -168,8 +168,7 @@ class Product extends Model {
 
 		$sql = new Sql();
 
-		return $sql->select("
-			SELECT * FROM tb_categories a INNER JOIN tb_productscategories b ON a.idcategory = b.idcategory WHERE b.idproduct = :idproduct
+		return $sql->select("SELECT * FROM tb_categories a INNER JOIN tb_productscategories b ON a.idcategory = b.idcategory WHERE b.idproduct = :idproduct
 		", [
 
 			':idproduct'=>$this->getidproduct()
